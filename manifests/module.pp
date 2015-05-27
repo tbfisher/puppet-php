23488a9a884b92adf588be45409c960f55e5b31d
Brian Fisher <tbfisher@gmail.com>
2015-05-27 05:44:57 -0700

    clean up

--- manifests/module.pp
+++ manifests/module.pp
@@ -1,39 +1,32 @@
 define php::module (
   $module = $title,
-  $package = '',
+  $package = "php5-${title}",
   $sapi = 'all',
 ) {
 
   require php
 
-  if $package {
-    $_package = $package
-  }
-  else {
-    $_package = "php5-$module"
-  }
-
-  package { $_package: }
+  package { $package: }
 
   # Packages are not necessarily enabled by default.
   if $php::php_enmod {
     if ($sapi == 'ALL' and member($php::sapi, 'cli')) or $sapi == 'cli' {
       exec { "php5enmod -s cli ${module}":
         creates => "/etc/php5/cli/conf.d/20-${module}.ini",
-        require => Package[$_package],
+        require => Package[$package],
       }
     }
     if ($sapi == 'ALL' and member($php::sapi, 'apache2')) or $sapi == 'apache2' {
       exec { "php5enmod -s apache2 ${module}":
         creates => "/etc/php5/apache2/conf.d/20-${module}.ini",
-        require => Package[$_package],
+        require => Package[$package],
         notify => Service['apache2'],
       }
     }
     if ($sapi == 'ALL' and member($php::sapi, 'fpm')) or $sapi == 'fpm' {
       exec { "php5enmod -s fpm ${module}":
         creates => "/etc/php5/fpm/conf.d/20-${module}.ini",
-        require => Package[$_package],
+        require => Package[$package],
         notify => Service['php5-fpm'],
       }
     }
